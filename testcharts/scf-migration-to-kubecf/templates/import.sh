6484fc8302c183306d2b7856facff3ebcc1bdd2a
Ettore Di Giacinto <edigiacinto@suse.com>
2020-10-14 16:01:16 +0200

    Try to workaround mysql issues and disabling pxc_strict

--- testcharts/scf-migration-to-kubecf/templates/import.sh
+++ testcharts/scf-migration-to-kubecf/templates/import.sh
@@ -2,41 +2,41 @@
 set -e
 ROOT_DIR=$PWD
 BACKUP_DIR=$PWD/backup
+kubectl scale --replicas=0 sts api -n {{.Values.namespaces.kubecf}} --timeout=20m
+kubectl scale --replicas=0 sts cc-worker -n {{.Values.namespaces.kubecf}} --timeout=20m
+kubectl scale --replicas=0 sts uaa -n {{.Values.namespaces.kubecf}} --timeout=20m
 
 kubectl exec -t database-0 --namespace {{.Values.namespaces.kubecf}} -- bash -c \
-	"/var/vcap/packages/mariadb/bin/mysql \
-  --defaults-file=/var/vcap/jobs/mysql/config/mylogin.cnf \
-  --socket /var/vcap/sys/run/pxc-mysql/mysqld.sock \
-  -e 'drop database uaadb; create database uaadb;'"
+	"mysql \
+  -e 'SET GLOBAL pxc_strict_mode=DISABLED;'"
+kubectl exec -t database-0 --namespace {{.Values.namespaces.kubecf}} -- bash -c \
+	"mysql \
+  -e 'drop database uaa; create database uaa;'"
 
 kubectl exec --stdin database-0 --namespace {{.Values.namespaces.kubecf}} -- bash -c \
-	'/var/vcap/packages/mariadb/bin/mysql \
-  --defaults-file=/var/vcap/jobs/mysql/config/mylogin.cnf \
-  --socket /var/vcap/sys/run/pxc-mysql/mysqld.sock \
-  uaadb' <$BACKUP_DIR/uaadb-src.sql
+	'mysql \
+  uaa' <$BACKUP_DIR/uaadb-src.sql
 
 kubectl cp $BACKUP_DIR/blobstore-src.tgz {{.Values.namespaces.kubecf}}/blobstore-0:/.
 
 kubectl exec --stdin --tty --namespace {{.Values.namespaces.kubecf}} blobstore-0 -- bash -l -c 'rm -rf /var/vcap/store/shared/* && tar xvf blobstore-src.tgz && rm blobstore-src.tgz'
 
 kubectl exec mysql-0 --namespace {{.Values.namespaces.kubecf}} -- bash -c \
-	"/var/vcap/packages/mariadb/bin/mysql \
-  --defaults-file=/var/vcap/jobs/mysql/config/mylogin.cnf \
-  --socket /var/vcap/sys/run/pxc-mysql/mysqld.sock \
-  -e 'drop database ccdb; create database ccdb;'"
+	"mysql \
+  -e 'drop database cloud_controller; create database cloud_controller;'"
 
 kubectl exec --stdin mysql-0 --namespace {{.Values.namespaces.kubecf}} -- bash -c \
-	'/var/vcap/packages/mariadb/bin/mysql \
-  --defaults-file=/var/vcap/jobs/mysql/config/mylogin.cnf \
-  --socket /var/vcap/sys/run/pxc-mysql/mysqld.sock \
-  ccdb' <$BACKUP_DIR/ccdb-src.sql
+	'mysql \
+  cloud_controller' <$BACKUP_DIR/ccdb-src.sql
 
-kubectl exec --namespace {{.Values.namespaces.kubecf}} api-group-0 -- bash -c \
+kubectl exec --namespace {{.Values.namespaces.kubecf}} api-0 -c cloud-controller-ng-cloud-controller-ng -- bash -c \
 	"source /var/vcap/jobs/cloud_controller_ng/bin/ruby_version.sh; \
 export CLOUD_CONTROLLER_NG_CONFIG=/var/vcap/jobs/cloud_controller_ng/config/cloud_controller_ng.yml; \
 cd /var/vcap/packages/cloud_controller_ng/cloud_controller_ng; \
 bundle exec rake rotate_cc_database_key:perform"
 
-kubectl delete pod api-0 --namespace {{.Values.namespaces.kubecf}}
-kubectl delete pod cc-worker-0 --namespace {{.Values.namespaces.kubecf}}
-kubectl delete pod cc-clock-0 --namespace {{.Values.namespaces.kubecf}}
+kubectl exec -t database-0 --namespace {{.Values.namespaces.kubecf}} -- bash -c \
+	"mysql \
+  -e 'SET GLOBAL pxc_strict_mode=ENFORCING;'"
+kubectl scale --replicas=1 sts api -n {{.Values.namespaces.kubecf}} --timeout=20m
+kubectl scale --replicas=1 sts cc-worker -n {{.Values.namespaces.kubecf}} --timeout=20m
